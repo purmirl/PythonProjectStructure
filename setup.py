0b12283196b219d0fee33715589e328d40edc3c6
PeTrA <33143731+purmirl@users.noreply.github.com>
2020-05-16 21:45:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-# Copyright 2020 PeTrA. All rights reserved.
-# Python Project Structure Repository
-
-# PythonProjectStructure/setup.py
+  """
+ Copyright 2020 PeTrA. All rights reserved.
+ . Python Project Structure Repository;
+"""
+# PythonProjectStructure/setup.py;
